25440b2adc73447f88e60c3b007fbf931214f0aa
BugHunter-X <askzenai@gmail.com>
2024-12-30 17:34:09 +0100

    Add about.js file

---
+++ about.js
@@ -0,0 +1,25 @@
+```javascript
+// pages/index.js
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to my Next.js app</h1>
+      <Link href="/about">
+        <a>Go to About page</a>
+      </Link>
+    </div>
+  );
+}
+```
+```javascript
+// pages/about.js
+
+export default function About() {
+  // This will cause an error if you do not have a layout.js file in the pages directory
+  return (
+    <h1>About page</h1>
+  );
+}
+```
